31029484cda63da9c14fbd0f7a8a1891bead20dc
Patrick Mockridge <patrick@engzig.com>
2016-05-15 18:10:34 +0100

    Updated search object
    s, not t

--- js/app.js
+++ js/app.js
@@ -5,37 +5,38 @@ $('.movie-list').empty();
 var searchYear = $('#year').val();
 var searchTitle = $('#search').val();
 var omdbOptions = {
-  y: searchYear,
-  t: searchTitle
+  //y: searchYear,
+  s: searchTitle
 };
 var omdbAPI = "http://www.omdbapi.com/?";
-var displayMovies = function (data) {
+function displayMovies (data) {
   console.log(data);
   var movieHTML = "";
-  if (data.Reponse == "True") {
+  //if (data.Reponse == "True") {
   $.each(data.Search, function(i, movie) {
     movieHTML += "<li><div class='poster-wrap'>";
-    if (typeof movie.Poster != "undefined") {
+    //if (typeof movie.Poster != "undefined") {
       movieHTML += "<img class='movie-poster' src=" + movie.Poster + ">";
-      }
-    else {
-      movieHTML += "<i class='material-icons poster-placeholder'>crop_original</i>";
-      }
+      //}
+    //else {
+      //movieHTML += "<i class='material-icons poster-placeholder'>crop_original</i>";
+      //}
     movieHTML += "</div>";
     movieHTML += "<span class='movie-title'>" + movie.Title + "</span>";
     movieHTML += "<span class='movie-year'>" + movie.Year + "</span>   </li>";
+    //}
   });
-  }
-  else if (data.Response == "False") {
-    movieHTML += "<li class='no-movies'> <i class='material-icons icon-help'>help_outline</i>No movies found that match: "
-    if (typeof searchYear != "undefined") {
-      movieHTML += searchTitle + " ";
-    }
-    if (typeof searchYear != "undefined") {
-      movieHTML += searchYear;
-    }
-  }
-  $('.movie-list').html(movieHTML);
+
+  //else if (data.Response == "False" || typeof data.Reponse == "undefined") {
+    //movieHTML += "<li class='no-movies'> <i class='material-icons icon-help'>help_outline</i>No movies found that match: "
+    //if (typeof searchYear != "undefined") {
+      //movieHTML += searchTitle + " ";
+    //}
+    //if (typeof searchYear != "undefined") {
+      //movieHTML += searchYear;
+    //}
+  //}
+  $('#movies').html(movieHTML);
 }
 $.getJSON(omdbAPI, omdbOptions, displayMovies);
 });
