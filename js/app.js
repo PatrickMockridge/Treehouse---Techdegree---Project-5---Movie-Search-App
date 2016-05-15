c84692b40857afc88634bfbc6bb299ef9cf15e25
Patrick Mockridge <patrick@engzig.com>
2016-05-15 13:04:37 +0100

    Made some changes
    Still won’t work though!! Ahh

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 'use strict'
 $(document).ready(function () {
   $('.search-button').click(function() {
-var searchYear = $('#year').text();
-var searchTitle = $('#search').text();
+var searchYear = $('#year').val();
+var searchTitle = $('#search').val();
 var omdbOptions = {
-  "y": searchYear,
-  "t": searchTitle
+  y: searchYear,
+  t: searchTitle
 };
 var omdbAPI = "http://www.omdbapi.com/?";
 var displayMovies = function (data) {
   console.log(data);
   var movieHTML = "";
-  if (data.reponse) {
+  if (data.Reponse == "True") {
   $.each(data.Search, function(i, movie) {
     movieHTML += "<li><div class='poster-wrap'>";
     if (typeof movie.Poster != "undefined") {
@@ -25,8 +25,14 @@ var displayMovies = function (data) {
     movieHTML += "<span class='movie-year'>" + movie.Year + "</span>   </li>";
   });
   }
-  else {
-    movieHTML += "<li class='no-movies'> <i class='material-icons icon-help'>help_outline</i>No movies found that match: [search form value].</li>"
+  else if (data.Response == "False") {
+    movieHTML += "<li class='no-movies'> <i class='material-icons icon-help'>help_outline</i>No movies found that match: "
+    if (typeof searchYear != "undefined") {
+      movieHTML += searchTitle + " ";
+    }
+    if (typeof searchYear != "undefined") {
+      movieHTML += searchYear;
+    }
   }
   $('.movie-list').html(movieHTML);
 }
