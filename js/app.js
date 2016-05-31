762797719cc7ed0eabb312c02dfdf763a363ced9
Patrick Mockridge <patrick@engzig.com>
2016-05-31 23:31:46 +0100

    Pagination of Search Results
    Search on keyup, pagination, removal of search button, bootstrap added
    and links added.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,37 @@
 'use strict'
 $(document).ready(function () {
-  $('.search-button').click(function() {
-    event.preventDefault();
+  //put pagination in a footer
+$('body').append('<footer><ul id="pagination"></ul></footer>');
+  //sticky footer css
+  $('html').css({
+      "position": "relative",
+      "min-height": "100%"
+  });
+  $('body').css({
+      "margin": "0 0 100px" /* bottom = footer height */
+  });
+  $('footer').css({
+      "text-align": "center",
+      "position": "absolute",
+      "left": "0",
+      "bottom": "0",
+      "height": "100px",
+      "width": "100%"
+  });
+// start page count
+// create function to retrieve JSON and update HTML accordingly
+var searchFunction = function(count) {
+event.preventDefault();
 $('.movie-list').empty();
+$('#pagination').empty();
 var searchYear = $('#year').val();
 var searchTitle = $('#search').val();
+var pageNo = String(count);
 var omdbOptions = {
   y: searchYear,
   s: searchTitle,
-  r: "json"
+  r: "json",
+  page: pageNo
 };
 var omdbAPI = "http://www.omdbapi.com/?";
 function displayMovies (data) {
@@ -17,7 +40,7 @@ function displayMovies (data) {
   $.each(data.Search, function(i, movie) {
     movieHTML += "<li><div class='poster-wrap'>";
     if (movie.Poster != "N/A") {
-      movieHTML += "<img class='movie-poster' src=" + movie.Poster + ">";
+      movieHTML += "<a href= http://www.imdb.com/title/" + movie.imdbID + " class='movie-poster'><img src=" + movie.Poster + "></a>";
       }
     else if (movie.Poster == "N/A") {
       movieHTML += "<i class='material-icons poster-placeholder'>crop_original</i>";
@@ -26,6 +49,17 @@ function displayMovies (data) {
     movieHTML += "<span class='movie-title'>" + movie.Title + "</span>";
     movieHTML += "<span class='movie-year'>" + movie.Year + "</span>   </li>";
     });
+    var totalResults = parseInt(data.totalResults);
+    var totalPages = Math.floor(totalResults/10)+1
+    if (totalResults > 10) {
+      $('#pagination').bootpag({
+        total: totalResults,
+        maxVisible: 5
+        }).on('page', function(event, num){
+        event.preventDefault();
+        searchFunction(num);
+      });
+      }
   }
   else if (data.Response == "False" || typeof data.Reponse == "undefined") {
     movieHTML += "<li class='no-movies'> <i class='material-icons icon-help'>help_outline</i>No movies found that match: "
@@ -39,5 +73,11 @@ function displayMovies (data) {
   $('#movies').html(movieHTML);
 }
 $.getJSON(omdbAPI, omdbOptions, displayMovies);
+//end of search function
+};
+$('input').keyup(function() {
+  event.preventDefault();
+  searchFunction(1);
 });
+//document ready close
 });
